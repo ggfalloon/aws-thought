82d44c0f3de8e697587f8f9b8e58e643c5520561
Gabriele Falloon <ggfalloon@gmail.com>
2021-05-20 14:39:38 -0500

    create thoughts table and schema

--- server/db/CreateThoughtsTable.js
+++ server/db/CreateThoughtsTable.js
@@ -3,24 +3,26 @@ const AWS = require("aws-sdk");
 // config points to local instance,
 // updates local environmental variables
 AWS.config.update({
-  region: "us-east-2",
-  endpoint: "http://localhost:8000"
+    region: "us-east-2",
+    endpoint: "http://localhost:8000"
 });
 
-const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
 
+
+// params object to hold the schema and metadata
 const params = {
-    TableName : "Thoughts",
-    KeySchema: [       
-        { AttributeName: "username", KeyType: "HASH"},  //Partition key
+    TableName: "Thoughts",
+    KeySchema: [
+        { AttributeName: "username", KeyType: "HASH" },  //Partition key
         { AttributeName: "createdAt", KeyType: "RANGE" }  //Sort key
     ],
-    AttributeDefinitions: [       
-        { AttributeName: "username", AttributeType: "S" },
-        { AttributeName: "createdAt", AttributeType: "N" }
+    AttributeDefinitions: [
+        { AttributeName: "username", AttributeType: "S" }, // s for string
+        { AttributeName: "createdAt", AttributeType: "N" } // n for number
     ],
-    ProvisionedThroughput: {       
-        ReadCapacityUnits: 10, 
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 10, // reserves a max read and write capacity, $$
         WriteCapacityUnits: 10
     }
 };
